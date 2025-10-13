9c335f26ca473a11ac2bf4b9ffa2bd52203758b8
AleksZ <alekszurowski@gmail.com>
2025-10-13 17:36:18 +0200

    location permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/azurowski/clarus/MainActivity.kt
+++ app/src/main/java/com/azurowski/clarus/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.azurowski.clarus
 
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,18 +19,23 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.azurowski.clarus.ui.theme.ClarusTheme
 import com.azurowski.clarus.WeatherApi
 import com.azurowski.clarus.ui.weather.WeatherUiState
 import com.azurowski.clarus.ui.weather.WeatherViewModel
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -56,6 +65,23 @@ class MainActivity : ComponentActivity() {
 fun WeatherApp(viewModel: WeatherViewModel = remember { WeatherViewModel(WeatherApi) }) {
     val state by viewModel.uiState.collectAsState()
 
+    val context = LocalContext.current
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    var latitude by remember { mutableStateOf<Double?>(null) }
+    var longitude by remember { mutableStateOf<Double?>(null) }
+
+    RequestLocationPermissions(
+        onPermissionGranted = {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    latitude = it.latitude
+                    longitude = it.longitude
+                }
+            }
+        }
+    )
+
+
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         when (state) {
             is WeatherUiState.Idle -> Text("Nie pobrano pogody")
@@ -68,11 +94,42 @@ fun WeatherApp(viewModel: WeatherViewModel = remember { WeatherViewModel(Weather
             is WeatherUiState.Error -> Text("Błąd: ${(state as WeatherUiState.Error).message}")
         }
 
-        Button(onClick = { viewModel.fetchWeather(50.06, 19.94) }) {
+        Button(
+            onClick = {
+                if(latitude != null && longitude != null) {
+                    viewModel.fetchWeather(latitude!!, longitude!!)
+                }
+            },
+            enabled = latitude != null && longitude != null
+        ) {
             Text("Pobierz pogodę")
         }
     }
 }
+
+@Composable
+fun RequestLocationPermissions(onPermissionGranted: () -> Unit) {
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true -> onPermissionGranted()
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true -> onPermissionGranted()
+            else -> Toast.makeText(context, "Brak dostępu do lokalizacji", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
