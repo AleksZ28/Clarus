767df99e05c9a7c59db3250a9932b8c19198a0f4
AleksZ <alekszurowski@gmail.com>
2025-10-18 19:53:04 +0200

    added weather icons and styling

--- app/src/main/java/com/azurowski/clarus/MainActivity.kt
+++ app/src/main/java/com/azurowski/clarus/MainActivity.kt
@@ -9,13 +9,17 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
@@ -29,9 +33,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,19 +94,46 @@ fun WeatherApp(viewModel: WeatherViewModel = remember { WeatherViewModel(Weather
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(brush = WeatherBackgrounds.Sunny)
+            .background(brush = WeatherBackgrounds.Night)
             .padding(
-                vertical = 20.dp
+                vertical = 30.dp
             )
             .statusBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(15.dp)
     ) {
+
         when (state) {
             is WeatherUiState.Idle -> Text("Nie pobrano pogody")
             is WeatherUiState.Loading -> CircularProgressIndicator()
             is WeatherUiState.Success -> {
                 val temp = round((state as WeatherUiState.Success).data.current.apparent_temperature).toInt()
-                Text("$temp°", color = WhiteTransparent, fontSize = 128.sp, fontWeight = FontWeight.Bold)
+                Image(
+                    painter = painterResource(R.drawable.moon),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(128.dp)
+                        .dropShadow(
+                            shape = RectangleShape,
+                            block = {
+                                radius = 1000f
+                                spread = 0.05f
+                                alpha = 0.25f
+                            }
+                        )
+                )
+                Text(
+                    text = "$temp°",
+                    style = TextStyle(
+                        color = WhiteTransparent,
+                        fontSize = 128.sp,
+                        fontWeight = FontWeight.Bold,
+                        shadow = Shadow(
+                            color = Color.Black.copy(alpha = 0.25f),
+                            blurRadius = 25f
+                        )
+                    )
+                )
             }
 
             is WeatherUiState.Error -> Text("Błąd: ${(state as WeatherUiState.Error).message}")
