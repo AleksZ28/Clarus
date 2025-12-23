6bb4c580a1bc1f3ffd41b48fda1a172e0ff47eeb
AleksZ <alekszurowski@gmail.com>
2025-12-23 16:26:43 +0100

    add pull to refresh

--- app/src/main/java/com/azurowski/clarus/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/azurowski/clarus/ui/screens/HomeScreen.kt
@@ -13,9 +13,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -55,9 +55,16 @@ import com.azurowski.clarus.ui.weather.parseNightWeather
 import com.google.android.gms.location.LocationServices
 import kotlin.math.round
 
+fun downloadWeather(latitude: Double?, longitude: Double?, viewModel: WeatherViewModel) {
+    if(latitude != null && longitude != null) {
+        viewModel.fetchWeather(latitude, longitude)
+    }
+}
 @Composable
 fun HomeScreen(navController: NavController, viewModel: WeatherViewModel = remember { WeatherViewModel(WeatherApi) }) {
     val state by viewModel.uiState.collectAsState()
+    val isRefreshing = state is WeatherUiState.Loading
+
 
     val context = LocalContext.current
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
@@ -75,119 +82,124 @@ fun HomeScreen(navController: NavController, viewModel: WeatherViewModel = remem
         }
     )
 
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = when (state) {
-                    is WeatherUiState.Success -> mapWeatherBackground((state as WeatherUiState.Success).data.current)
-                    else -> WeatherBackgrounds.Sunny
-                }
-            )
-            .verticalScroll(rememberScrollState())
-            .padding(
-                vertical = 30.dp
-            )
-            .statusBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(15.dp)
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = { downloadWeather(latitude, longitude, viewModel) },
+        modifier = Modifier.fillMaxSize()
     ) {
-
-        when (state) {
-            is WeatherUiState.Idle -> Text("Nie pobrano pogody")
-            is WeatherUiState.Loading -> CircularProgressIndicator()
-            is WeatherUiState.Success -> {
-                val currentWeather = (state as WeatherUiState.Success).data.current
-                val currentTemp = round(currentWeather.apparent_temperature).toInt()
-                val currentWeatherType = mapCurrentWeather(currentWeather)
-                val currentWeatherIcon = getWeatherIcon(currentWeatherType)
-
-                Image(
-                    painter = painterResource(currentWeatherIcon),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(128.dp)
-                        .dropShadow(
-                            shape = RectangleShape,
-                            block = {
-                                radius = 1000f
-                                spread = 0.05f
-                                alpha = 0.25f
-                            }
-                        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = when (state) {
+                        is WeatherUiState.Success -> mapWeatherBackground((state as WeatherUiState.Success).data.current)
+                        else -> WeatherBackgrounds.Sunny
+                    }
                 )
-                Text(
-                    text = if (currentTemp >= 0) " $currentTemp°" else "$currentTemp°",
-                    style = TextStyle(
-                        color = WhiteTransparent,
-                        fontSize = 128.sp,
-                        fontWeight = FontWeight.Bold,
-                        shadow = Shadow(
-                            color = Color.Black.copy(alpha = 0.25f),
-                            blurRadius = 25f
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    vertical = 30.dp
+                )
+                .statusBarsPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+
+            when (state) {
+                is WeatherUiState.Idle -> Text("Nie pobrano pogody")
+                is WeatherUiState.Loading -> CircularProgressIndicator()
+                is WeatherUiState.Success -> {
+                    val currentWeather = (state as WeatherUiState.Success).data.current
+                    val currentTemp = round(currentWeather.apparent_temperature).toInt()
+                    val currentWeatherType = mapCurrentWeather(currentWeather)
+                    val currentWeatherIcon = getWeatherIcon(currentWeatherType)
+
+                    Image(
+                        painter = painterResource(currentWeatherIcon),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(128.dp)
+                            .dropShadow(
+                                shape = RectangleShape,
+                                block = {
+                                    radius = 1000f
+                                    spread = 0.05f
+                                    alpha = 0.25f
+                                }
+                            )
+                    )
+                    Text(
+                        text = if (currentTemp >= 0) " $currentTemp°" else "$currentTemp°",
+                        style = TextStyle(
+                            color = WhiteTransparent,
+                            fontSize = 128.sp,
+                            fontWeight = FontWeight.Bold,
+                            shadow = Shadow(
+                                color = Color.Black.copy(alpha = 0.25f),
+                                blurRadius = 25f
+                            )
                         )
                     )
-                )
 
-                val hourlyWeatherFromApi = (state as WeatherUiState.Success).data.hourly
+                    val hourlyWeatherFromApi = (state as WeatherUiState.Success).data.hourly
 
-                val nextNightWeather = mapNextNight(hourlyWeatherFromApi)
-                val parsedNextNightWeather = parseNightWeather(nextNightWeather)
-                Log.d("NIGHT_WEATHER", parsedNextNightWeather.toString())
+                    val nextNightWeather = mapNextNight(hourlyWeatherFromApi)
+                    val parsedNextNightWeather = parseNightWeather(nextNightWeather)
+                    Log.d("NIGHT_WEATHER", parsedNextNightWeather.toString())
 
-                val nextDayWeather = mapNextDay(hourlyWeatherFromApi, latitude!!, longitude!!)
-                val parsedNextDayWeather = parseDayWeather(nextDayWeather)
-                Log.d("DAY_WEATHER", parsedNextDayWeather.toString())
+                    val nextDayWeather = mapNextDay(hourlyWeatherFromApi, latitude!!, longitude!!)
+                    val parsedNextDayWeather = parseDayWeather(nextDayWeather)
+                    Log.d("DAY_WEATHER", parsedNextDayWeather.toString())
 
-                val isCurrentDayNight = nextDayWeather[0].isCurrentDayNight!!
+                    val isCurrentDayNight = nextDayWeather[0].isCurrentDayNight!!
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 30.dp),
-                    horizontalArrangement = Arrangement.spacedBy(20.dp)
-                ) {
-                    Label(text = if (!isCurrentDayNight) "Noc" else "Nadchodzący dzień", modifier = Modifier.weight(1f))
-                    Label(text = if (!isCurrentDayNight) "Jutro" else "Następna noc", modifier = Modifier.weight(1f))
-                }
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 30.dp, end = 30.dp, bottom = 20.dp),
-                    horizontalArrangement = Arrangement.spacedBy(20.dp)
-                ) {
-                    if (!isCurrentDayNight){
-                        WeatherCard(modifier = Modifier.weight(1f), false, parsedNextNightWeather)
-                        WeatherCard(modifier = Modifier.weight(1f), true, parsedNextDayWeather)
-                    } else {
-                        WeatherCard(modifier = Modifier.weight(1f), true, parsedNextDayWeather)
-                        WeatherCard(modifier = Modifier.weight(1f), false, parsedNextNightWeather)
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp),
+                        horizontalArrangement = Arrangement.spacedBy(20.dp)
+                    ) {
+                        Label(text = if (!isCurrentDayNight) "Noc" else "Nadchodzący dzień", modifier = Modifier.weight(1f))
+                        Label(text = if (!isCurrentDayNight) "Jutro" else "Następna noc", modifier = Modifier.weight(1f))
                     }
 
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, end = 30.dp, bottom = 20.dp),
+                        horizontalArrangement = Arrangement.spacedBy(20.dp)
+                    ) {
+                        if (!isCurrentDayNight){
+                            WeatherCard(modifier = Modifier.weight(1f), false, parsedNextNightWeather)
+                            WeatherCard(modifier = Modifier.weight(1f), true, parsedNextDayWeather)
+                        } else {
+                            WeatherCard(modifier = Modifier.weight(1f), true, parsedNextDayWeather)
+                            WeatherCard(modifier = Modifier.weight(1f), false, parsedNextNightWeather)
+                        }
+
+                    }
+
+                    Label("Prognoza godzinowa")
+
+                    val hourlyWeather = mapNext24Hours(hourlyWeatherFromApi)
+
+                    HourlyWeatherRow(hourlyWeather)
+
                 }
 
-                Label("Prognoza godzinowa")
-
-                val hourlyWeather = mapNext24Hours(hourlyWeatherFromApi)
-
-                HourlyWeatherRow(hourlyWeather)
-
+                is WeatherUiState.Error -> Text("Błąd: ${(state as WeatherUiState.Error).message}")
             }
 
-            is WeatherUiState.Error -> Text("Błąd: ${(state as WeatherUiState.Error).message}")
-        }
-
-        Button(
-            onClick = {
-                if(latitude != null && longitude != null) {
-                    viewModel.fetchWeather(latitude!!, longitude!!)
-                }
-            },
-            enabled = latitude != null && longitude != null
-        ) {
-            Text("Pobierz pogodę")
+//            Button(
+//                onClick = {
+//                    if(latitude != null && longitude != null) {
+//                        viewModel.fetchWeather(latitude!!, longitude!!)
+//                    }
+//                },
+//                enabled = latitude != null && longitude != null
+//            ) {
+//                Text("Pobierz pogodę")
+//            }
         }
     }
 }
