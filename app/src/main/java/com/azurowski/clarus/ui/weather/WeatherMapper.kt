229e61bd81f6d017ac983db5c8af82593c1084b7
AleksZ <alekszurowski@gmail.com>
2025-12-24 14:56:48 +0100

    a slight change in WeatherMapper logic

--- app/src/main/java/com/azurowski/clarus/ui/weather/WeatherMapper.kt
+++ app/src/main/java/com/azurowski/clarus/ui/weather/WeatherMapper.kt
@@ -34,12 +34,12 @@ fun mapHourlyWeather(hourly: HourlyWeatherToProcess): String{
     val is_day = hourly.is_day
 
     return when {
-        snowfall > 0.1 && precipitation_probability > 40 -> "snowy"
+        snowfall > 0.1 && precipitation_probability > 30 -> "snowy"
         precipitation_probability > 40 && rain > 2.5 -> "heavy_rain"
         precipitation_probability > 40 && rain > 0.75 -> "mid_rain"
         precipitation_probability > 40 && rain > 0.3 -> "low_rain"
         precipitation_probability > 40 && showers > 0.3 -> "low_rain"
-        precipitation_probability in 30..<40 -> "low_rain"
+        precipitation_probability in 20..<40 -> "low_rain"
         cloud_cover >= 75 -> "cloudy"
         is_day == 0 -> "night"
         cloud_cover >= 50 -> "75_cloudy"
